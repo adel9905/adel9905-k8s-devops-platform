8672d2149bc085582787b97b78a5dc4a4bcc904b
adel9905 <adeltech9905@gmail.com>
2026-01-19 12:14:16 +0100

    Make SonarQube public subnet with internet acces1s

--- tf_code/sonarqube-ec2.tf
+++ tf_code/sonarqube-ec2.tf
@@ -18,19 +18,3 @@ resource "aws_instance" "sonarqube" {
     Name = "${local.env}-sonarqube"
   }
 }
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "${local.env}-public-rt"
-  }
-}
-resource "aws_route_table_association" "public_az1" {
-  subnet_id      = aws_subnet.public_az1.id
-  route_table_id = aws_route_table.public.id
-}
